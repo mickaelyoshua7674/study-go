8cdb4f85243098b68f1276c739434be642fce93f
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2025-01-04 10:13:48 -0300

    finish section 10

--- Section 10 - Concurrency - Running Tasks In Parallel/main.go
+++ Section 10 - Concurrency - Running Tasks In Parallel/main.go
@@ -5,16 +5,26 @@ import (
 )
 
 func main() {
-    greet("Nice to meet you!")
-    greet("How are you?")
-    slowGreet("How ... are ... you ...?")
-    greet("I hope you're liking the course!")
+    /*
+    Channels are pipes of data for comunication between goroutines.
+    */
+    done := make(chan bool)
+
+    go greet("Nice to meet you!", done)
+    go greet("How are you?", done)
+    go slowGreet("How ... are ... you ...?", done)
+    go greet("I hope you're liking the course!", done)
+
+    for range done {}
 }
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
     fmt.Println("Hello!", phrase)
+    doneChan <- true
 }
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
     time.Sleep(3*time.Second)
-    greet(phrase)
+    greet(phrase, doneChan)
+    doneChan <- true
+    close(doneChan)
 }
